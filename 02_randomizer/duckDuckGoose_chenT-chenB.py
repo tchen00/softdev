f465c801f679da77693b050a1cfcb3f0363239ed
Tammy Chen <tchen00@stuy.edu>
2019-09-11 20:42:47 -0400

    add in comments & code

--- 02_randomizer/duckDuckGoose_chenT-chenB.py
+++ 02_randomizer/duckDuckGoose_chenT-chenB.py
@@ -1,3 +1,8 @@
+# Duck Duck Goose -- Tammy Chen and Brandon Chen
+# SoftDev1 pd9
+# K #02: NO-body expects the Spanish Inquisition!
+# 2019-09-11
+
 import random
 
 # dictionary from class to test code later on!
@@ -6,3 +11,10 @@ KREWES = {
         'rex': ['William', 'Joseph', 'Calvin', 'Ethan', 'Moody', 'Mo', 'Big Mo', 'Peihua', 'Saad', 'Benjamin', 'Justin', 'Alice', 'Hilary', 'Ayham', 'Michael', 'Matthew', 'Jionghao', 'Devin ', 'David', 'Jacob', 'Will', 'Hannah', 'Alex'],
         'endymion': ['Grace', 'Nahi', 'Derek', 'Jun Tao', 'Connor', 'Jason', 'Tammy', 'Albert', 'Kazi', 'Derek', 'Brandon', 'Kenneth', 'Lauren', 'Biraj', 'Jeff', 'Jackson', 'Taejoon', 'Kevin', 'Jude', 'Sophie', 'Henry', 'Coby', 'Manfred', 'Leia', 'Ahmed', 'Winston']
 }
+
+# Given the team, the function will choose a random student from that team!
+def randomStudent(team):
+    # the team can be one of three -- 'orpheus', 'rex', 'endymion'
+    return random.choice(KREWES[team])
+
+#print randomStudent('orpheus')
