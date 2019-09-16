1947ab01f6786aaf387116824ce68969907c6058
Tammy Chen <tchen00@stuy.edu>
2019-09-16 09:20:42 -0400

    add to dictionary

--- duckDuckGoose_chenT_chenB.py
+++ duckDuckGoose_chenT_chenB.py
@@ -4,5 +4,20 @@ list = open('occupations.csv', 'r')
 list = list.read()
 list = list.split('\n')
 
+# Removing first row!
 list.pop(0)
 total = float(list.pop(-2).split(',')[1])
+
+for i in range(len(list)):
+    if list[i] == '':
+        list.pop(i)
+    elif list[i].count(',') > 1:
+        list[i] = list[i].split('",')
+    else:
+        list[i] = list[i].split(',')
+
+occupations = {}
+for l in list:
+    occupations[l[0]] = float(l[1])
+
+print occupations
