c69a089689417b77bf1d7de2ed2be5c550d2fc31
Tammy Chen <tchen00@stuy.edu>
2019-09-21 12:13:27 -0400

    got table to work! + random value + used henrys code

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -1,41 +1,23 @@
 from flask import Flask, render_template
-from random import random
+# HENRY'S CODE!
+import csv
+import random
 
-# Reading CSV file + splitting by new line!
-data = open('occupations.csv', 'r')
-data = data.read()
-data = data.split('\n')
+workersAndPercent = {}
+with open('occupations.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    for row in readCSV: #for each row in the csv file
+        if row[0] != 'Job Class': #as long as it's not the first row
+            workersAndPercent[row[0]] = float(row[1]) #turn value into a float
+    workersAndPercent.pop('Total',99) #pop off the last row
 
-# Removing first row!
-data.pop(0)
-total = float(data.pop(-2).split(',')[1]) #getting the total which is 99.8!
+def randomO(d):
+    bigL = list()
+    for key,value in d.items():
+        bigL += [key] * int(value * 10) #add each job to the list value*10 many times
+    return random.choice(bigL) #weighted random choice based on how many times a job is in the list
 
-# Reading through the data
-for i in range(len(data)):
-    if data[i] == '':
-        data.pop(i)
-    elif data[i].count(',') > 1:
-        data[i] = data[i].split('",')
-    else:
-        data[i] = data[i].split(',')
-
-# Transferring everything into the dictionary
-occupations = {}
-for l in data:
-    occupations[l[0]] = float(l[1])
-
-def random_average():
-    # generate random number
-    bar = 0 # interval for each occupation
-    randvalue = random() * total # INTERVAL [0,99.8)
-    keys = list(occupations.keys())
-    for key in keys:
-        value = occupations[key]
-        bar += value
-        if (randvalue <= bar):
-            return key
-
-
+# ASSIGNMENT
 app = Flask(__name__) #create instance of class Flask
 
 @app.route("/") #assign following fxn to run when roo route requested
@@ -47,8 +29,8 @@ def hello_word():
 def test_tmplt():
     return render_template(
     'table_tmplt.html',
-    foo = "fun",
-    collection = occupations)
+    randOcc = randomO(workersAndPercent),
+    collection = workersAndPercent)
 
 
 if __name__ == "__main__":
