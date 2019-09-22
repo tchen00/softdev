46684b21e5ae688f32464798844be27edb06aa30
Tammy Chen <tchen00@stuy.edu>
2019-09-22 18:09:32 -0400

    update code from henrys -- he got links to work

--- 10_occ/app.py
+++ 10_occ/app.py
@@ -8,14 +8,14 @@ with open('occupations.csv') as csvfile:
     readCSV = csv.reader(csvfile, delimiter=',')
     for row in readCSV: #for each row in the csv file
         if row[0] != 'Job Class': #as long as it's not the first row
-            workersAndPercent[row[0]] = float(row[1]) #turn value into a float
+            workersAndPercent[row[0]] = [float(row[1]),row[2]] #turn value into a float
             #workersAndPercent[row[0]] = row[2]
     workersAndPercent.pop('Total',99) #pop off the last row
 
 def randomO(d):
     bigL = list()
     for key,value in d.items():
-        bigL += [key] * int(value * 10) #add each job to the list value*10 many times
+        bigL += [key] * int(value[0] * 10) #add each job to the list value*10 many times
     return random.choice(bigL) #weighted random choice based on how many times a job is in the list
 
 # ASSIGNMENT
