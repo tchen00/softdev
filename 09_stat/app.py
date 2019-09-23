d3ea17338375231202ab86bbfaa3207fec45fcfe
Tammy Chen <tchen00@stuy.edu>
2019-09-23 14:25:35 -0400

    reorganize 09

--- 09_stat/app.py
+++ 09_stat/app.py
@@ -1,22 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__) #create instance of class Flask
-#app2 = Flask(__name2__)
 
-# 1
-@app.route("/static/pair") #assign following fxn to run when roo route requested
+@app.route("/") #assign following fxn to run when roo route requested
 def hello_word():
-    print(__name__)
-    coll = {0,1,1,2,3,5,8}
-    return {{coll}}
-    # print(__name2__)
+    return "Hello World"
 
-    #return {{coll}} #HTML
+coll = {0,1,1,2,3,5,8}
+@app.route("/my_foist_template")
+    #return "hello"
+def test_tmplt():
+    return render_template(
+    'model_tmplt.html',
+    foo = "fun",
+    collection = coll)
 
 
-
-
-# Run!
 if __name__ == "__main__":
     # will automatically update without running again
     app.debug = True
