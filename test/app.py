6a2fb62fa58882367ed3156d3ea802ad267ddb62
Tammy Chen <tchen00@stuy.edu>
2019-10-03 14:32:04 -0400

    added gitignroe

--- test/app.py
+++ test/app.py
@@ -1,13 +1,36 @@
+# Tammy Chen & Leia Park
+# SoftDev1 pd9
+# K12 -- Echo Echo Echo
+# 2019-09-26
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/")
+def home():
+    #return "hello"
+    #return render_template(
+    #    'request.html'
+    #)
+    #return "hello"
+    return render_template("request.html") #displays the form
+
 @app.route("/auth")
 def authenticate():
+    '''
+    print("//////////////|\\\\\\\\\\\\\\")
+    print("****DIAG: request obj ****")
+    print(request)
     print(app)
+    print("****DIAG: request args obj ****")
     print(request.args)
+    '''
+    #redirect to the response.html & input the parameters
     return render_template(
-        'foo.html'
+        'response.html',
+        username = request.args['username'],
+        method = request.method
     )
 
 if __name__ == "__main__":
