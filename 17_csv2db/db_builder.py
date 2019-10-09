8c4995e0e852a638d9b107a6df18907593c5a79d
Tammy Chen <tchen00@stuy.edu>
2019-10-09 14:20:20 -0400

    basically done but need to fix one more thing -- u

--- 17_csv2db/db_builder.py
+++ 17_csv2db/db_builder.py
@@ -1,28 +1,51 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev  
-#skeleton :: SQLITE3 BASICS
-#Oct 2019
+#Tammy Chen
+#SoftDev1 pd9
+#K17: No Trouble
+#2019-10-10
 
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitate CSV I/O
 
 
-DB_FILE="discobandit.db"
+DB_FILE="data.db"
 
 db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
 c = db.cursor()               #facilitate db ops
 
 #==========================================================
 
-# < < < INSERT YOUR POPULATE-THE-DB CODE HERE > > >
+with open('courses.csv') as course:
+    reader = csv.DictReader(course)
 
+    command = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER);"
+    c.execute(command)
 
+    for row in reader:
+        instr = "INSERT INTO courses VALUES ('{}', {}, {});".format(row['code'], row['mark'], row['id'])
+        c.execute(instr)
+
+with open('students.csv') as student:
+    reader = csv.DictReader(student)
+
+    command = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER);"
+    c.execute(command)
+
+    for row in reader:
+        instr = "INSERT INTO students VALUES ('{}', {}, {});".format(row['name'], row['age'], row['id'])
+        c.execute(instr)
+
+"""
 command = ""          # test SQL stmt in sqlite3 shell, save as string
 c.execute(command)    # run SQL statement
+"""
 
+# TESTING CODE!!!
+c.execute("SELECT * FROM students;")
+c.execute("SELECT * FROM courses;")
+rows = c.fetchall()
+for row in rows:
+    print(row)
 #==========================================================
 
 db.commit() #save changes
 db.close()  #close database
-
-
