88e3eebf6089338fc898f5e14297af7a203ce559
Tammy Chen <tchen00@stuy.edu>
2019-12-10 14:54:21 -0500

    finished two fxn

---
+++ 27_js/funky_town.js
@@ -0,0 +1,25 @@
+/*
+var foo = function(n){
+  return n;
+};
+*/
+
+var factorial = function(n){
+  if (n == 1) return 1;
+  return (n * factorial(n-1));
+};
+
+
+var fibonacci = function(n){
+  if (n < 2) return n;
+  if (n == 2) return 1;
+  return (fibonacci(n-1) + fibonacci(n-2));
+};
+
+var gcd = function(a,b){
+  if (a < b){
+    return gcd(b, a);
+  }
+  if (a % b == 0) return b;
+  return gcd(a-b, b);
+}
