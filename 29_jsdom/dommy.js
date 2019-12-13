10fd8e0809e67a576d60226475f119cb98187d5b
Tammy Chen <tchen00@stuy.edu>
2019-12-13 14:32:32 -0500

    add comments

--- 29_jsdom/dommy.js
+++ 29_jsdom/dommy.js
@@ -8,28 +8,34 @@ K29: Sequential Progression III: Season of the Witch
 var original = document.getElementById("h").innerHTML;
 
 var changeHeading = function(e) {
-  var h = document.getElementById("h");
-  h.innerHTML = e['target']['innerHTML'];
+    var h = document.getElementById("h");
+    // target property of the event is a reference to the object that dispatched the event
+    // in this case, it would be the header
+    h.innerHTML = e['target']['innerHTML'];
 };
 
 var removeItem = function(e) {
-  document.getElementById('thelist').removeChild(e['target'])
+    // accesses the element with given ID and removes the child at that reference of the object
+    document.getElementById('thelist').removeChild(e['target'])
 };
 
 var restoreText = function(e){
-  var h = document.getElementById("h")
-  h.innerHTML = original;
+    // allows header to return to original "Hello World"
+    var h = document.getElementById("h")
+    h.innerHTML = original;
 };
 
 var lis = document.getElementsByTagName("li");
 
 for (var i=0; i < lis.length; i++) {
+    // given the action, will know what function to do
     lis[i].addEventListener('mouseover', changeHeading);
     lis[i].addEventListener('mouseout', restoreText);
     lis[i].addEventListener('click', removeItem);
 };
 
 var addItem = function(e) {
+    // adds new element "WØ‰D" into list
     var list = document.getElementById('thelist');
     var item = document.createElement("li");
     item.innerHTML = "WORD";
@@ -39,38 +45,43 @@ var addItem = function(e) {
     list.appendChild( item );
 };
 
+// every time button with id 'b' is pressed, add new item
 var button = document.getElementById("b");
 button.addEventListener('click', addItem);
 
+// fibonacci function from last assignment
 var fibonacci = function(n){
-  if (n < 2) return n;
-  if (n == 2) return 1;
-  return (fibonacci(n-1) + fibonacci(n-2));
+    if (n < 2) return n;
+    if (n == 2) return 1;
+    return (fibonacci(n-1) + fibonacci(n-2));
 };
 
 var fib
 var addFib = function(e){
-  var fiblist = document.getElementById('fiblist');
-  var newfib = document.createElement('li');
-  newfib.innerHTML = fibonacci(fib_counter);
-  fib_counter++;
-  fiblist.appendChild( newfib );
+    // adds next fibonacci into the list
+    var fiblist = document.getElementById('fiblist');
+    var newfib = document.createElement('li');
+    newfib.innerHTML = fibonacci(fib_counter);
+    fib_counter++;
+    fiblist.appendChild( newfib );
 };
 
 var addFib2 = function(e){
-  var fiblist = document.getElementById('fiblist');
-  var newfib = document.createElement('li');
-  var children = fiblist.childNodes;
-  var length = children.length;
-  if (length < 3){
-    newfib.innerHTML = 1;
-  }
-  else{
-    newfib.innerHTML = parseInt(children[length - 1].innerHTML , 10) + parseInt(children[length - 2].innerHTML, 10);
-  }
-  console.log(children);
-  fiblist.appendChild( newfib );
- };
+    // allows for fib not to have a delay
+    var fiblist = document.getElementById('fiblist');
+    var newfib = document.createElement('li');
+    var children = fiblist.childNodes;
+    var length = children.length;
+    if (length < 3){
+      newfib.innerHTML = 1;
+    }
+    else{
+      // gets the last two fibs from the list (rather than a recursive loop, runs simple addition)
+      newfib.innerHTML = parseInt(children[length - 1].innerHTML , 10) + parseInt(children[length - 2].innerHTML, 10);
+    }
+    //console.log(children); 
+    fiblist.appendChild( newfib );
+};
 
 var fb = document.getElementById("fb");
 fb.addEventListener("click", addFib2);
