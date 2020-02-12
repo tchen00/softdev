ebd70dfcf355b630521df6d39fbf115a250ad2f2
Tammy Chen <tchen00@stuy.edu>
2020-02-12 14:41:24 -0500

    updated team name & fix bugs

--- 07_canvas-anim/animation.js
+++ 07_canvas-anim/animation.js
@@ -1,4 +1,4 @@
-// Tammy Chen & Grace Mao (Team Muhkolyk)
+// Tammy Chen & Grace Mao (Team wiggles)
 // SoftDev2 pd9
 // K07: They lock us in the tower whenever we get caught
 // 2020-02-12
@@ -12,9 +12,9 @@ var stopButton = document.getElementById('stop');
 var growth = 1; // increment value
 var requestID = 0;
 var radius = 5;
+var moving = 0;
 
 ctx.fillStyle = "#00ffff";
-
 
 var clear_canvas = function(){
   ctx.clearRect(0,0,500,500);
@@ -32,22 +32,33 @@ var drawDot = function(){
   radius += growth;
   clear_canvas();
   ctx.beginPath();
-  ctx.arc( canvas.width / 2, canvas.height/2, radius, 0, 2 * Math.PI);
+  ctx.arc(canvas.width / 2, canvas.height/2, radius, 0, 2 * Math.PI);
   ctx.stroke();
   ctx.fill();
-  requestID = window.requestAnimationFrame(drawDot); //starts an animation frame request
+  if (requestID != 0) {
+    console.log(requestID);
+    requestID = window.requestAnimationFrame(drawDot); //starts an animation frame request
+  }
 };
 
 var stopIt = function(){
   window.cancelAnimationFrame(requestID); //cancels an animation frame request
   growth = 0;
+  moving = 0;
+  requestID = 0;
 };
 
 dotButton.addEventListener('click', function(e){
-  if (growth == 0){
-    growth = 1;
+  if (moving == 0) {
+    requestID = window.requestAnimationFrame(drawDot);
+    if (growth == 0){
+      growth = 1;
+      drawDot();
+    }
+    //console.log(growth);
     drawDot();
+    moving = 1;
   }
-  drawDot();
 });
+
 stopButton.addEventListener('click', stopIt);
