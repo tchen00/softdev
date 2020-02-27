be9d494c20b19482471de14b033c170094eab830
Tammy Chen <tchen00@stuy.edu>
2020-02-27 16:36:41 -0500

    work on stuff

---
+++ 09_mongo/mongo.py
@@ -0,0 +1,47 @@
+# Team red - Tammy Chen & Kiran Vuksanaj
+# SoftDev pd9
+# K09: Yummy Mongo Py
+# 2020-02-28
+
+from json import loads
+from pymongo import MongoClient
+
+client  = MongoClient()  # sets up clients
+db = client.test # sets up database
+restaurants = db.restaurants # restaurants database
+
+# reading json file into database
+if (restaurants.count() == 0):
+    file = open("primer-dataset.json", "r")
+    content = file.readlines()
+    for line in content:
+        restaurants.insert_one(loads(line))
+
+def select_borough(borough):
+    """ Returns all restaurants in a specified borough. """
+    return restaurants.find({"borough" : borough})
+
+def select_zipCode(zipCode):
+    """ Returns all restaurants in a specified zip code. """
+    return restaurants.find({"address.zipcode" : zipCode})
+
+def select_zip_grade(zipcode, grade):
+    ''' Returns all restaurants in a specified zip code and with a specified grade.'''
+    return restaurants.find({"address.zipcode" : zipcode, "grades.grade" : grade})
+
+def select_zip_score(zipcode, score):
+    '''Returns all restaurants in a specified zip code with a score below a specified threshold.'''
+    return restaurants.find({"address.zipcode": zipcode, "grades.score" : {"$lt" : score}})
+
+def select_num_zip(zipcode, number):
+    '''Returns specified number of restaurants in given zip code.'''
+    return restaurants.find({"address.zipcode": zipcode}).limit(number)
+
+print("--------------TESTING------------------")
+
+print("------------ FINDING ALL RESTAURANTS IN STATEN ISLAND ------------")
+for rest in select_borough("Staten Island"):
+    for key, value in rest.items():
+        if key == "name":
+            print("{name: %s}" % value)
+    #print(rest)
