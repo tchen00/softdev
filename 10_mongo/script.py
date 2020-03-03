f4d3ede8681a88e36e7633daedb9f16047c7b1c1
Tammy Chen <tchen00@stuy.edu>
2020-03-03 14:19:59 -0500

    update nobel

--- 10_mongo/script.py
+++ 10_mongo/script.py
@@ -3,24 +3,26 @@
 # K10: Import/Export Bank
 # 2020-03-04
 
+import pprint
 from json import loads
 from pymongo import MongoClient
 
 client  = MongoClient()  # sets up clients
 db = client.test # sets up database
-history = db.history # restaurants database
+nobel = db.history # restaurants database
 
 # reading json file into database
-if (history.count() == 0):
-    file = open("history.json", "r")
+if (nobel.count() == 0):
+    file = open("nobel.json", "r")
     content = file.readlines()
     for line in content:
-        history.insert_one(loads(line))
+        nobel.insert_one(loads(line))
 
 def event_date(date):
     print(date)
-    for i in history.result.find({"event.date": date}):
-        pprint.pprint(i)
-        print("hello")
-        
+    #pprint.pprint(nobel.result)
+    #for i in history.result.find({"event.date": date}):
+    #    pprint.pprint(i)
+    #    print("hello")
+
 event_date("-300")
