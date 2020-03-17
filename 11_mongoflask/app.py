2ff6ecde16527001cb888ba151a7a50a37cec19c
Tammy Chen <tchen00@stuy.edu>
2020-03-16 20:57:12 -0400

    finished

---
+++ 11_mongoflask/app.py
@@ -0,0 +1,61 @@
+# Team sleep - Tammy Chen & Alice Ni
+# SoftDev pd9
+# K11: Ay Mon Go Git It From Yer Flask
+#2020-03-16
+
+'''
+The dataset contains all nobel prize winners & their research. It includes information such as the year & category of the award.
+LINK: http://api.nobelprize.org/v1/prize.json
+For our import mechanism, we read the dataset into a list and then used the imported json library to convert the dataset into a dictionary, which is then inserted into the database. We also imported pprint, which makes the information easier to read when printed.
+'''
+
+import pymongo, urllib.request, json
+from flask import Flask, render_template, request, url_for, redirect
+
+connection = pymongo.MongoClient("localhost:27017")
+db = connection.prize
+collection = db.nobel_prize
+
+f = open("nobel.json")
+data = json.load(f)
+collection.remove()
+collection.insert_many(data["prizes"])
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template("/index.html",
+                            subject = findCategory("chemistry"),
+    )
+
+def findCategory(category):
+    output = []
+    for i in collection.find({"category" : category}):
+        output.append(i)
+    return output
+
+def findYear(year):
+    output = []
+    for i in collection.find({"year" : year}):
+        output.append(i)
+    return output
+
+def findSurname(surname):
+    output = []
+    for i in collection.find({"laureates.surname" : surname}):
+        output.append(i)
+    return output
+
+@app.route("/year", methods = ["POST"])
+def year():
+    try:
+        year = request.form.get("year")
+        return render_template("/index.html",
+                                subject = findCategory("chemistry"),
+                                year = findYear(year))
+    except:
+        return redirect(url_for("home"))
+
+if __name__=="__main__":
+    app.debug = True
+    app.run()
