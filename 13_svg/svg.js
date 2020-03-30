1ff3dc7b4c3c5d41213e36e6868aea15ebbeaa9f
Tammy Chen <tchen00@stuy.edu>
2020-03-30 14:43:19 -0400

    almost' g

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -5,36 +5,41 @@
 
 var svg = document.getElementById("vimage");
 var clear = document.getElementById("clear");
-var path = null;
 
-svg.addEventListener('click', function(e) {
-  //createElementNS: Creates an element with the specified namespace URI and qualified name.
-  var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-  var xcor = e.offsetX // mouse xcor
-  var ycor = e.offsetY // mouse ycor
-  c.setAttribute("cx", xcor); // circle center xcor
-  c.setAttribute("cy", ycor); // circle center ycor
-  c.setAttribute("r", "10"); // circle radius
-  c.setAttribute("fill", "blue");
-  c.setAttribute("stroke", "black");
-  svg.appendChild(c);
-  if (path){
-    // drawing line
-    var a = document.createElementNS("http://www.w3.org/2000/svg", "line");
-    a.setAttribute("x1", path.x);
-    a.setAttribute("y1", path.y);
-    a.setAttribute("x2", xcor);
-    a.setAttribute("y2", ycor);
-    a.setAttribute("stroke", "black");
-    svg.appendChild(a);
-  }
-  path = {x: xcor, y: ycor};
+function add(x, y){
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", 50);
+    c.setAttribute("fill", "blue");
+    c.setAttribute("stroke", "black");
+    return c;
 }
-);
 
-clear.addEventListener('click', function(e){
-  while (svg.pathChild) {
-    svg.removeChild(svg.pathChild);
-    path = null;
+function randLocation(min, max){
+    return Math.floor((max - min) * Math.random()) + min;
 }
+
+svg.addEventListener("click", function(e){
+    if(e.target.nodeName == "svg"){
+        svg.appendChild(draw(e.clientX, e.clientY));
+    }
 });
+
+svg.addEventListener("click", function(e){
+    if(e.target.nodeName == "circle"){
+        if(e.target.getAttribute("fill") == "blue"){
+            e.target.setAttribute("fill", "green");
+            e.target.setAttribute("stroke", "green");
+        } else {
+            svg.removeChild(e.target);
+            svg.appendChild(draw(randLocation(0, 500), randLocation(0, 500)));
+        }
+    }
+});
+
+clear.addEventListener('click', function(e)){
+  while(pic.firstChild){
+      pic.removeChild(pic.firstChild);
+  }
+};
