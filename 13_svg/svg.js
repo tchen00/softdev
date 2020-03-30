53de2d1e36d7e0ec3b30e9269afdd76fe810be3d
Tammy Chen <tchen00@stuy.edu>
2020-03-30 15:30:52 -0400

    bruh howd i break the clear button

--- 13_svg/svg.js
+++ 13_svg/svg.js
@@ -1,45 +1,41 @@
-// Team "." - Tammy Chen & Joseph Lee
-// SoftDev pd9
-// K12: Connect the Dots
-// 2020-03-30
-
-var svg = document.getElementById("vimage");
+var pic = document.getElementById('vimage');
 var clear = document.getElementById("clear");
+pic.innerHTML = '';
 
-function add(x, y){
-    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    c.setAttribute("cx", x);
-    c.setAttribute("cy", y);
-    c.setAttribute("r", 50);
-    c.setAttribute("fill", "blue");
-    c.setAttribute("stroke", "black");
-    return c;
+var dot = (x,y) => {
+  var c = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+  c.setAttribute("cx", x);
+  c.setAttribute("cy", y);
+  c.setAttribute("r", 15);
+  c.setAttribute("fill", "blue");
+  c.setAttribute("stroke", "black");
+  c.addEventListener('click', function(e) {
+    changeColor(e, c); // adds color & delete
+  });
+  return c;
 }
 
-function randLocation(min, max){
-    return Math.floor((max - min) * Math.random()) + min;
-}
-
-svg.addEventListener("click", function(e){
-    if(e.target.nodeName == "svg"){
-        svg.appendChild(draw(e.clientX, e.clientY));
-    }
-});
-
-svg.addEventListener("click", function(e){
-    if(e.target.nodeName == "circle"){
-        if(e.target.getAttribute("fill") == "blue"){
-            e.target.setAttribute("fill", "green");
-            e.target.setAttribute("stroke", "green");
-        } else {
-            svg.removeChild(e.target);
-            svg.appendChild(draw(randLocation(0, 500), randLocation(0, 500)));
-        }
-    }
-});
-
-clear.addEventListener('click', function(e)){
-  while(pic.firstChild){
-      pic.removeChild(pic.firstChild);
-  }
+var draw = (e) => {
+  var c = dot(e.offsetX, e.offsetY);
+  pic.appendChild(c);
 };
+
+
+var changeColor = (e, c) => {
+  if (c.getAttribute('fill') == 'blue'){
+    // first click -- change color
+    c.setAttribute('fill', 'cyan');
+  }
+  else if (c.getAttribute('fill') == 'cyan'){
+    // second click -- random location
+    var randchild = dot(Math.random() * 500, Math.random() * 500);
+    pic.appendChild(randchild);
+    c.remove();
+  }
+}
+
+var clear = (e) => {
+  pic.innerHTML='';
+};
+
+clear.addEventListener('click', clear);
