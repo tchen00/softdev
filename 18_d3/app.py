9c43feda00587852abf880022069dd19619b44c5
Tammy Chen <tchen00@stuy.edu>
2020-04-18 16:44:07 -0400

    working on it

---
+++ 18_d3/app.py
@@ -0,0 +1,17 @@
+# Team hallo - Tammy Chen & Grace Mao
+# SoftDev pd9
+# K18: Come Up For Air
+# 2020-04-21
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+
+def root():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
