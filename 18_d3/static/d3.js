4d46753913be9f62a8e9ef7f65e6b7916106071c
Tammy Chen <tchen00@stuy.edu>
2020-04-18 17:40:16 -0400

    getting there

---
+++ 18_d3/static/d3.js
@@ -0,0 +1,7 @@
+// Team hallo - Tammy Chen & Grace Mao
+// SoftDev pd9
+// K18: Come Up For Air
+// 2020-04-21
+
+var renderbtn = document.getElementById('render');
+var transitionbtn = document.getElementById('transition');
