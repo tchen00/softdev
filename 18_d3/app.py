f4951f9b0ee60e7fac694a210898b4634399024e
Tammy Chen <tchen00@stuy.edu>
2020-04-19 19:43:10 -0400

    add from grace

--- 18_d3/app.py
+++ 18_d3/app.py
@@ -4,13 +4,33 @@
 # 2020-04-21
 
 from flask import Flask, render_template
+import csv
 
 app = Flask(__name__)
 
-@app.route("/")
+def parse():
+    dict = {}
+    with open('covid.csv', 'r') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if (row[0][0] != 'D'): # not the first row
+                # date: <string>, values: <array> [new cases, hospitalized, deaths]
+                #print(row)
+                #print('DONE')
+                #print(type(row[0]))
+                dict[row[0]] = [int(row[1]), int(row[2]), int(row[3])]
+        #print(dict)
+    return dict
 
+@app.route("/")
 def root():
-    return render_template('index.html')
+    d = parse()
+    #print(d)
+    dates = list(d.keys())
+    nums = list(d.values())
+    print(dates)
+    print(nums)
+    return render_template('index.html', dates=dates, csv=nums)
 
 if __name__ == "__main__":
     app.debug = True
--- 18_d3/static/d3.js
+++ 18_d3/static/d3.js
@@ -3,5 +3,84 @@
 // K18: Come Up For Air
 // 2020-04-21
 
+//var yes = "we";
 var renderbtn = document.getElementById('render');
 var transitionbtn = document.getElementById('transition');
+
+var render = function(e){
+  const new_cases = [];
+
+  var i;
+  for (i = 0; i < data.length; i++) {
+    new_cases.push(data[i][0]);
+  };
+  //console.log(x(new_cases[0]) + 50);
+  //var n = create(new_cases);
+  //document.getElementsByTagName("body")[0].append(n);
+  create(new_cases);
+}
+
+
+var transition = function(e){
+  const hosp = [];
+  var i;
+  for (i = 0; i < data.length; i++) {
+    hosp.push(data[i][1]);
+  };
+
+  d3.select("body").transition(create(hosp));
+
+};
+
+
+const create = function(data){
+    console.log(data);
+    var width = 1000;
+    var x = d3.scaleLinear()
+      .domain([0, d3.max(data)])
+      .range([0, width]);
+    var y = d3.scaleBand()
+      .domain(d3.range(data.length))
+      .range([0, 15 * data.length]);
+
+  const svg = d3.create("svg")
+      .attr("width", width + 200)
+      .attr("height", y.range()[1])
+      .attr("font-family", "sans-serif")
+      .attr("font-size", "10")
+      .attr("text-anchor", "end");
+
+  const bar = svg.selectAll("g")
+    .data(data)
+    .join("g")
+      .attr("transform", (d, i) => `translate(0,${y(i)})`);
+
+  bar.append("rect")
+      .attr("fill", "steelblue")
+      .attr("width", x)
+      .attr("height", y.bandwidth() - 1);
+
+  bar.append("text")
+      .attr("fill", "black")
+      .attr("x", d => x(d + 225))
+      .attr("y", y.bandwidth() / 2 + 3)
+      .attr("dy", "0.35")
+      .text(function(d, i) {
+        return heading[i];
+      });
+
+  bar.append("text")
+      .attr("fill", "white")
+      .attr("x", d => x(d - 25))
+      .attr("y", y.bandwidth() / 2 + 3)
+      .attr("dy", "0.35")
+      .text(function(d) {
+        return d;
+      });
+
+  document.getElementsByTagName("body")[0].append(svg.node());
+}
+
+
+renderbtn.addEventListener('click', render);
+transitionbtn.addEventListener('click', transition);
